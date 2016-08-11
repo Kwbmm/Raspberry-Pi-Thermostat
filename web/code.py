e397667884324f218b7255f411576984a8bcd48a
Marco Ardizzone <kwbmm1990@gmail.com>
2016-08-11 22:38:55 +0200

    Test on folder structure

--- web/code.py
+++ web/code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import web
-from routes.index import Index
+from ..routes.index import Index
 
 if __name__ == "__main__":
 	app = web.application(('/*', 'Index'), globals())
