a5dc5b17b1ecabfad3566d2704559e678a52afdc
Marco Ardizzone <kwbmm1990@gmail.com>
2016-08-15 00:18:24 +0200

    Add JS copied from P&L Thermostat project. Not working..

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,30 @@
-$(document).foundation()
+$(document).ready(function(){
+	$(document).foundation();
+//	var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+//	switch(page){
+//		case 'setProgram':
+//		setProgram();
+//		break;
+//		case 'stats':
+//		stats();
+//		break;
+//		default:
+//		break;
+//	}
+});
+
+$(window).load(function() {
+	SetOffCanvasHeight();
+}).resize(function() {
+	SetOffCanvasHeight();
+});
+
+function SetOffCanvasHeight() {
+	var height = $(window).height();
+	var contentHeight = $(".off-canvas-content").height();
+	if (contentHeight > height) { height = contentHeight; }
+
+	$(".off-canvas-wrapper").height(height);
+	$(".off-canvas-wrapper-inner").height(height);
+	$(".off-canvas").height(height);
+}
