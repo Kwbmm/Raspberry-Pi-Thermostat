24568d1d91b7468375b680bb13c714aa11eed8d0
Marco Ardizzone <kwbmm1990@gmail.com>
2016-08-17 19:30:58 +0200

    Optimize query to get data only for the weekday we are looking at

--- routes/Program.py
+++ routes/Program.py
@@ -8,32 +8,29 @@ render = render_jinja('templates', encoding='utf-8')
 
 class Program():
 	def GET(self, day):
-		data = self.__getWeekDays()
-		data["day"] = day
-		return render.program(data=data, domain=web.ctx.homedomain)
+		data = self.__getWeekDays(day)
+		return render.program(data=data, day=day, domain=web.ctx.homedomain)
 
-	def __getWeekDays(self):
-		sql = "SELECT weekDay, hour, targetTemp FROM program ORDER BY weekDay, hour"
+	def __getWeekDays(self, day):
+		if day == "monday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 1 ORDER BY hour"
+		elif day == "tuesday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 2 ORDER BY hour"
+		elif day == "wednesday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 3 ORDER BY hour"
+		elif day == "thursday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 4 ORDER BY hour"
+		elif day == "friday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 5 ORDER BY hour"
+		elif day == "saturday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 6 ORDER BY hour"
+		elif day == "sunday":
+			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 7 ORDER BY hour"
 		conn = sqlite3.connect("thermostat.db")
 		cur = conn.cursor()
 		cur.execute(sql)
 		result = cur.fetchall()
-		returnData = {}
+		returnData = []
 		for row in result:
-			if row[0] == 1:
-				returnData["Monday"] = {'hour': row[1], 'temp': row[2]}
-			elif row[0] == 2:
-				returnData["Tuesday"] = {'hour': row[1], 'temp': row[2]}
-			elif row[0] == 3:
-				returnData["Wednesday"] = {'hour': row[1], 'temp': row[2]}
-			elif row[0] == 4:
-				returnData["Thursday"] = {'hour': row[1], 'temp': row[2]}
-			elif row[0] == 5:
-				returnData["Friday"] = {'hour': row[1], 'temp': row[2]}
-			elif row[0] == 6:
-				returnData["Saturday"] = {'hour': row[1], 'temp': row[2]}
-			elif row[0] == 7:
-				returnData["Sunday"] = {'hour': row[1], 'temp': row[2]}
-			else:
-				returnData["error"] = True
+			returnData.append(row[1])
 		return returnData
