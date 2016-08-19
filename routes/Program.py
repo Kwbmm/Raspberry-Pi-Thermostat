a10e277a8222bebf7c17db42de7b5b64ecb00f62
Marco Ardizzone <kwbmm1990@gmail.com>
2016-08-20 00:09:21 +0200

    POST management is more or less done. It's missing error management and transaction rollback

--- routes/Program.py
+++ routes/Program.py
@@ -13,26 +13,72 @@ class Program():
 			return render.program(data=None, day=day, domain=web.ctx.homedomain)
 		return render.program(data=data, day=day, domain=web.ctx.homedomain)
 
+	def POST(self, day):
+		data = web.input()
+		num = self.__weekDayString2Num(day)
+		sql = "SELECT targetTemp FROM program WHERE weekDay = ? AND hour = ?"
+		conn = sqlite3.connect("thermostat.db")
+		output = []
+		conn.execute("BEGIN EXCLUSIVE TRANSACTION")
+		for rawHour, temp in data.items():
+			hour = rawHour.split("-")
+			hour = hour[1]
+			cur = conn.cursor()
+			cur.execute(sql, (num, hour))
+			result = cur.fetchall()
+			if len(result) == 0:  # There is no data in the DB
+				insertSql = "INSERT INTO program (weekDay,hour,targetTEmp) VALUES(?,?,?)"
+				cur.execute(insertSql, (num, hour, temp))
+			else:
+				updateSql = "UPDATE program SET targetTemp = ? WHERE weekDay = ? AND hour = ?"
+				cur.execute(updateSql, (temp, num, hour))
+		conn.commit()
+		return render.program(data=self.__getWeekDays(day), day=day, domain=web.ctx.homedomain)
+
 	def __getWeekDays(self, day):
-		if day == "monday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 1 ORDER BY hour"
-		elif day == "tuesday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 2 ORDER BY hour"
-		elif day == "wednesday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 3 ORDER BY hour"
-		elif day == "thursday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 4 ORDER BY hour"
-		elif day == "friday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 5 ORDER BY hour"
-		elif day == "saturday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 6 ORDER BY hour"
-		elif day == "sunday":
-			sql = "SELECT hour, targetTemp FROM program WHERE weekDay = 7 ORDER BY hour"
+		num = self.__weekDayString2Num(day)
+		sql = "SELECT hour, targetTemp FROM program WHERE weekDay = ? ORDER BY hour"
 		conn = sqlite3.connect("thermostat.db")
 		cur = conn.cursor()
-		cur.execute(sql)
+		cur.execute(sql, (num,))
 		result = cur.fetchall()
 		returnData = []
 		for row in result:
 			returnData.append(row[1])
 		return returnData
+
+	def __weekDayNum2String(self, day):
+		if day == 1:
+			return 'monday'
+		elif day == 2:
+			return 'tuesday'
+		elif day == 3:
+			return 'wednesday'
+		elif day == 4:
+			return 'thursday'
+		elif day == 5:
+			return 'friday'
+		elif day == 6:
+			return 'saturday'
+		elif day == 7:
+			return 'sunday'
+		else:
+			return None
+
+	def __weekDayString2Num(self, day):
+		if day == 'monday':
+			return 1
+		elif day == 'tuesday':
+			return 2
+		elif day == 'wednesday':
+			return 3
+		elif day == 'thursday':
+			return 4
+		elif day == 'friday':
+			return 5
+		elif day == 'saturday':
+			return 6
+		elif day == 'sunday':
+			return 7
+		else:
+			return None
