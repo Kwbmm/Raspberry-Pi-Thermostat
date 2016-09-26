5520897292950a54c70fc3072cf2c6ea6d1b41c7
Marco Ardizzone <kwbmm1990@gmail.com>
2016-09-27 00:06:27 +0200

    Remove infinite loop in Main

--- Main.py
+++ Main.py
@@ -11,5 +11,4 @@ btnUp.when_pressed = display.increaseTargetTemp
 btnDown = Button(7)
 btnDown.when_pressed = display.decreaseTargetTemp
 with ThermostatSensor(18, 23) as t:
-	while True:
-		display.updateScreen(t.getTemp())
+	display.updateScreen(t.getTemp())
