d83f7ced98c29cb4fa063973fe87f5dd9124c5b4
Marco Ardizzone <kwbmm1990@gmail.com>
2016-10-14 12:29:26 +0200

    Add missing comma

--- run.py
+++ run.py
@@ -3,11 +3,12 @@
 import web
 from routes.Home import Home
 from routes.Program import Program
+from routes.Login import Login
 
 urls = (
-    '/', 'Home',
-    '/program/(monday|tuesday|wednesday|thursday|friday|saturday|sunday)',
-    'Program'
+    "/", "Home",
+    "/program/(monday|tuesday|wednesday|thursday|friday|saturday|sunday)", "Program",
+    "/login", "Login"
 )
 
 if __name__ == "__main__":
